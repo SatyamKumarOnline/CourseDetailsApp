ef530c53ac679323ed3ed7c9750447d4fdddea8d
Satyam Kumar <satyam.kumar@PROD.HCLPNP.COM>
2019-09-06 14:23:02 +0530

    ng-If else code changes.

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -28,4 +28,9 @@ export class CourseCardComponent implements OnInit {
     this.courseSelected.emit(this.course);
   }
 
+
+  isImageVisible() {
+    return this.course && this.course.iconUrl;
+  }
+
 }
