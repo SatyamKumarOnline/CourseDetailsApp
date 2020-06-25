44a4f0ba5c476504c345c9be1d0b37051600c79b
Satyam Kumar <satyam.kumar@prod.hclpnp.com>
2020-06-25 14:16:59 +0530

    View child with component ref and elementRef

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
-import {COURSES} from '../db-data';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { COURSES } from '../db-data';
+import { CourseCardComponent } from './course-card/course-card.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   courseList = COURSES;
+
+  @ViewChild(CourseCardComponent, { static: false })
+  courseItem: CourseCardComponent;
+
+  @ViewChild('cardRef1', { static: false, read: ElementRef })
+  courseItemElemRef: ElementRef;
+
+  @ViewChild('testContainer', { static: false })
+  testContainerElemRef: ElementRef;
+
   selectedCourse(course) {
     debugger;
-    console.log(11,course);
+    console.log(11, this.courseItem);
+    console.log(11, this.testContainerElemRef);
+    console.log(22, this.courseItemElemRef);
   }
 }
